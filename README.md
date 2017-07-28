05f907f1b3bade90f2ebc45c4ee05bfbc2f49da5
zeekhoo-okta <zeekhoo@gmail.com>
2017-07-27 23:47:34 -0700

    implemented all verbs

--- README.md
+++ README.md
@@ -1,9 +1,98 @@
+# Introduction
+Based on the [okta-scim-beta](https://github.com/oktadeveloper/okta-scim-beta) project,
+this example shows how to use Okta's cloud-based SCIM connector to automatically provision 
+and deprovision users managed by Okta into a single Cognito user-pool.
 
-# Understanding of User Provisioning in Okta
+This example code was written for **Python 2.7** and uses the excellent [Boto python interface
+to AWS](https://boto3.readthedocs.io/en/latest/guide/quickstart.html), which has support for Cognito.
+
+
+# Running the sample project
+
+`git checkout` this repository, then:
+
+1.  `cd` to the directory you just checked out:
+
+        $ cd okta-scim-to-cognito
+2.  Create an isolated Python environment named "venv" using [virtualenv](http://docs.python-guide.org/en/latest/dev/virtualenvs/):
+
+        $ virtualenv venv
+3.  Next, activate the newly created virtualenv:
+
+        $ source venv/bin/activate
+4.  Then, install the dependencies for the sample SCIM server using
+    Python's ["pip" package manager](https://en.wikipedia.org/wiki/Pip_%28package_manager%29):
+
+        $ pip install -r requirements.txt
+5.  Rename .sample.config.json to .sample.json
+
+        $ mv .sample.config.json .sample.json
+
+6.  Edit .sample.json and provide the values for user_pool_id, access_key and secret_key.
+    This project only provisions into a single user-pool, so you must supply the name for
+    that user-pool. 
+    Boto requires AWS credentials in order to make requests. Create security credentials
+    for an IAM user that has the AmazonCognitoPowerUser policy and enter the access_key
+    and secret_key values from the credentials into this file.
+    
+7.  Start the example SCIM server using this command:
+
+        $ python scim-server.py
+
+8.  Make this service reachable by the internet with [ngrok](https://ngrok.com/). 
+    Download and install ngrok, then start it up
+
+        $ ngrok http 5000
+
+## AWS Cognito User-pool requirements
+The sample project provisions and deprovisions users into a single congnito user-pool.
+At the minimum, the pool must have the following attributes:
+1.  email
+2.  name
+
+For simplicity's sake, the sample project creates users staged with
+the temporary password '123456'. In your user-pool's policy configuration: 
+1.  Set password minimum length = 6
+2.  Do not require special characters, uppercase or lowercase letters
+ 
+
+
+## Okta setup
+If you don't already have an Okta org, create an 
+[Okta Developer account](https://www.okta.com/developer/signup/). 
+
+Once you have access to your account, from the Admin UI:
+1.  Navigate to Applications > Applications. 
+Click on the green button [Add Application]
+ 
+2.  In the search box, search for "SCIM 2.0 Test App (Header Auth)"
+and click [Add]
+
+3.  During the Provisioning setup step, check the "Enable Provisioning features" box.
+
+4.  In the SCIM 2.0 Base Url field, enter https://your-ngrok-https-forwarding-url/scim/v2
+
+    e.g https://d0aafa9e.ngrok.io/scim/v2
+    
+5.  Enter any random value for API Token. The field is required, 
+    but the sample project will not be using it. In production, protect your
+    endpoints with an API Key. (Or Basic Auth or OAuth2. Recall from the previous steps, 
+    there are SCIM templates for each of the 3 types of auth protocols)
+
+6.  Click [Test API Credentials]. 
+
+If you receive a success message in the last step above, your configuration
+is complete. Now you can assign and unassign users to your app and watch it
+automatically provision and deprovision users to your Cognito user-pool.
+
+
+
+# Appendix
+## Understanding of User Provisioning in Okta
 
 Okta is a universal directory with the main focus in storing
 identity related information.  Users can be created in Okta directly
-as local users or imported from external systems like Active
+as local users or imported from external system like Active
 Directory or a [Human Resource Management Software](https://en.wikipedia.org/wiki/Category:Human_resource_management_software) system.
 
 An Okta user schema contains many different user attributes,
@@ -24,7 +113,7 @@ In Okta, an application instance is a connector that provides Single Sign-On
 and provisioning functionality with the target application.
 
 
-# Required SCIM Capabilities
+## Required SCIM Capabilities
 
 Okta supports provisioning to both SCIM 1.1 and SCIM 2.0 APIs.
 
@@ -42,7 +131,7 @@ Specifically, you do not need to implement the SCIM 2.0
 specification fully to work with Okta. At a minimum, Okta requires that
 your SCIM 2.0 API implement the features described below:
 
-## Base URL
+### Base URL
 
 The API endpoint for your SCIM API **MUST** be secured via [TLS](https://tools.ietf.org/html/rfc5246)
 (`https://`), Okta *does not* connect to unsecured API endpoints.
@@ -53,7 +142,7 @@ as your Base URL; for example: `https://example.com/scim/v2` -
 however, you must support the URL structure described in the
 ["SCIM Endpoints and HTTP Methods" section of RFC7644](https://tools.ietf.org/html/rfc7644#section-3.2).
 
-## Authentication
+### Authentication
 
 Your SCIM API **MUST** be secured against anonymous access. At the
 moment, Okta supports authentication against SCIM APIs with one of
@@ -63,7 +152,7 @@ the following methods:
 2.  [Basic Authentication](https://en.wikipedia.org/wiki/Basic_access_authentication)
 3.  Custom HTTP Header
 
-## Basic User Schema
+### Basic User Schema
 
 Your service must be capable of storing the following four user
 attributes:
--- scim-server.py
+++ scim-server.py
@@ -2,37 +2,33 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright © 2016, Okta, Inc.
-# 
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-# 
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import flask
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import url_for
 import re
+import json
 import boto3
 
 
-USER_POOL_ID = 'us-west-2_iloZUQ8lJ'
-
 app = Flask(__name__)
+
+config = None
+with open('.config.json') as config_file:
+    config_json = json.load(config_file)
+    config = config_json['config']
+
+USER_POOL_ID = config['aws']['user_pool_id']
+ACCESS_KEY = config['aws']['access_key']
+SECRET_KEY = config['aws']['secret_key']
+
+cognito_client = boto3.client(
+    'cognito-idp',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
 
 
 class ListResponse():
@@ -59,6 +55,7 @@ class ListResponse():
         return rv
 
 
+# Conforms the cognito user object to a SCIM format
 class CognitoUser:
     def __init__(self, resource):
         self.update(resource)
@@ -76,7 +73,6 @@ class CognitoUser:
             for pair in resource['UserAttributes']:
                 if pair['Name'] == 'name':
                     setattr(self, 'displayName', pair['Value'])
-
         setattr(self, 'displayName', displayName)
 
     def to_scim_resource(self):
@@ -100,41 +96,6 @@ class CognitoUser:
         return rv
 
 
-class User:
-    def __init__(self, resource):
-        self.update(resource)
-
-    def update(self, resource):
-        for attribute in ['userName', 'active']:
-            if attribute in resource:
-                setattr(self, attribute, resource[attribute])
-        for attribute in ['givenName', 'middleName', 'familyName']:
-            if attribute in resource['name']:
-                setattr(self, attribute, resource['name'][attribute])
-
-    def to_scim_resource(self):
-        rv = {
-            "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
-            "id": self.userName,
-            "userName": self.userName,
-            "name": {
-                "familyName": self.familyName,
-                "givenName": self.givenName,
-                "middleName": self.middleName
-            },
-            "active": self.active,
-            "meta": {
-                "resourceType": "User",
-                "location": url_for('user_get',
-                                    user_id=self.userName,
-                                    _external=True),
-                # "created": "2010-01-23T04:56:22Z",
-                # "lastModified": "2011-05-13T04:42:34Z",
-            }
-        }
-        return rv
-
-
 def scim_error(message, status_code=500):
     rv = {
         "schemas": ["urn:ietf:params:scim:api:messages:2.0:Error"],
@@ -144,28 +105,10 @@ def scim_error(message, status_code=500):
     return flask.jsonify(rv), status_code
 
 
-# def send_to_browser(obj):
-#     socketio.emit('user',
-#                   {'data': obj},
-#                   broadcast=True,
-#                   namespace='/test')
-
-
 def render_json(obj):
     user = CognitoUser(obj)
     rv = user.to_scim_resource()
     return flask.jsonify(rv)
-
-
-# @socketio.on('connect', namespace='/test')
-# def test_connect():
-#     for user in User.query.filter_by(active=True).all():
-#         emit('user', {'data': user.to_scim_resource()})
-#
-#
-# @socketio.on('disconnect', namespace='/test')
-# def test_disconnect():
-#     print('Client disconnected')
 
 
 @app.route('/')
@@ -176,12 +119,10 @@ def hello():
 @app.route("/scim/v2/Users/<user_id>", methods=['GET'])
 def user_get(user_id):
     try:
-        cognito_cli = boto3.client('cognito-idp')
-        response = cognito_cli.admin_get_user(
+        response = cognito_client.admin_get_user(
             UserPoolId=USER_POOL_ID,
             Username=user_id
         )
-        print('response = {}'.format(response))
     except:
         return scim_error("User not found", 404)
     return render_json(response)
@@ -190,13 +131,11 @@ def user_get(user_id):
 @app.route("/scim/v2/Users", methods=['POST'])
 def users_post():
     user_resource = request.get_json(force=True)
-    print('user_resource = {}'.format(user_resource))
 
     username = user_resource['userName']
     name = user_resource['name']['givenName'] + ' ' + user_resource['name']['familyName']
 
-    cognito_cli = boto3.client('cognito-idp')
-    response = cognito_cli.admin_create_user(
+    response = cognito_client.admin_create_user(
         UserPoolId=USER_POOL_ID,
         Username=username,
         UserAttributes=[
@@ -208,66 +147,82 @@ def users_post():
         MessageAction='SUPPRESS',
         DesiredDeliveryMediums=['EMAIL']
     )
-    print('response = {}'.format(response))
     user = CognitoUser(response['User'])
     rv = user.to_scim_resource()
     resp = flask.jsonify(rv)
     resp.headers['Location'] = url_for('user_get', user_id=username, _external=True)
     return resp, 201
 
-# @app.route("/scim/v2/Users", methods=['POST'])
-# def users_post():
-#     user_resource = request.get_json(force=True)
-#     user = User(user_resource)
-#     user.id = str(uuid.uuid4())
-#     db.session.add(user)
-#     db.session.commit()
-#     rv = user.to_scim_resource()
-#     send_to_browser(rv)
-#     resp = flask.jsonify(rv)
-#     resp.headers['Location'] = url_for('user_get',
-#                                        user_id=user.userName,
-#                                        _external=True)
-#     return resp, 201
 
-# to-do
-# @app.route("/scim/v2/Users/<user_id>", methods=['PUT'])
-# def users_put(user_id):
-#     user_resource = request.get_json(force=True)
-#     user = User.query.filter_by(id=user_id).one()
-#     user.update(user_resource)
-#     db.session.add(user)
-#     db.session.commit()
-#     return render_json(user)
+@app.route("/scim/v2/Users/<user_id>", methods=['PUT'])
+def users_put(user_id):
+    user_resource = request.get_json(force=True)
+    # TODO: Implement update user attributes
+
+    try:
+        user = cognito_client.admin_get_user(
+            UserPoolId=USER_POOL_ID,
+            Username=user_id
+        )
+    except:
+        return scim_error("User not found", 404)
+
+    return render_json(user)
 
 
-# to-do
-# @app.route("/scim/v2/Users/<user_id>", methods=['PATCH'])
-# def users_patch(user_id):
-#     patch_resource = request.get_json(force=True)
-#     for attribute in ['schemas', 'Operations']:
-#         if attribute not in patch_resource:
-#             message = "Payload must contain '{}' attribute.".format(attribute)
-#             return message, 400
-#     schema_patchop = 'urn:ietf:params:scim:api:messages:2.0:PatchOp'
-#     if schema_patchop not in patch_resource['schemas']:
-#         return "The 'schemas' type in this request is not supported.", 501
-#     user = User.query.filter_by(id=user_id).one()
-#     for operation in patch_resource['Operations']:
-#         if 'op' not in operation and operation['op'] != 'replace':
-#             continue
-#         value = operation['value']
-#         for key in value.keys():
-#             setattr(user, key, value[key])
-#     db.session.add(user)
-#     db.session.commit()
-#     return render_json(user)
+@app.route("/scim/v2/Users/<user_id>", methods=['PATCH'])
+def users_patch(user_id):
+    patch_resource = request.get_json(force=True)
+    for attribute in ['schemas', 'Operations']:
+        if attribute not in patch_resource:
+            message = "Payload must contain '{}' attribute.".format(attribute)
+            return message, 400
+    schema_patchop = 'urn:ietf:params:scim:api:messages:2.0:PatchOp'
+    if schema_patchop not in patch_resource['schemas']:
+        return "The 'schemas' type in this request is not supported.", 501
+
+    deactivate = None
+    reactivate = None
+    for operation in patch_resource['Operations']:
+        if 'op' not in operation and operation['op'] != 'replace':
+            continue
+        value = operation['value']
+        for key in value.keys():
+            if key == 'active':
+                val = str(value[key])
+                if val == ''.join('False'):
+                    deactivate = True
+                else:
+                    reactivate = True
+    if deactivate:
+        try:
+            response = cognito_client.admin_disable_user(
+                UserPoolId=USER_POOL_ID,
+                Username=user_id
+            )
+        except:
+            return scim_error("User not found", 404)
+    if reactivate:
+        try:
+            response = cognito_client.admin_enable_user(
+                UserPoolId=USER_POOL_ID,
+                Username=user_id
+            )
+        except:
+            return scim_error("User not found", 404)
+
+    try:
+        user = cognito_client.admin_get_user(
+            UserPoolId=USER_POOL_ID,
+            Username=user_id
+        )
+    except:
+        return scim_error("User not found", 404)
+    return render_json(user)
 
 
-# to-do
 @app.route("/scim/v2/Users", methods=['GET'])
 def users_get():
-    print('request = {}'.format(request))
     count = int(request.args.get('count', 100))
     start_index = int(request.args.get('startIndex', 1))
     if start_index < 1:
@@ -276,21 +231,25 @@ def users_get():
 
     match = None
     filter = None
+    search_key_name = None
     request_filter = request.args.get('filter')
+    # Handling the filter users requirement...
+    # see more info at: https://github.com/oktadeveloper/okta-scim-beta#filtering-on-id-username-and-emails
     if request_filter:
         match = re.match('(\w+) eq "([^"]*)"', request_filter)
     if match:
         (search_key_name, search_value) = match.groups()
         if search_key_name == 'userName':
             search_key_name = 'username'
-        else:
-            search_key_name = search_key_name.lower()
+        elif search_key_name == 'emails':
+            search_key_name = 'email'
+        elif search_key_name == 'id':
+            search_key_name = 'username'
+    if search_key_name:
         filter = search_key_name + ' = ' + '"' + search_value + '"'
 
-    print('filter = {}'.format(filter))
-    cognito_cli = boto3.client('cognito-idp')
     if filter:
-        response = cognito_cli.list_users(
+        response = cognito_client.list_users(
             UserPoolId=USER_POOL_ID,
             AttributesToGet=[
                 'name', 'email'
@@ -299,7 +258,7 @@ def users_get():
             Filter=filter
         )
     else:
-        response = cognito_cli.list_users(
+        response = cognito_client.list_users(
             UserPoolId=USER_POOL_ID,
             AttributesToGet=[
                 'name', 'email'
@@ -308,11 +267,7 @@ def users_get():
         )
 
     found = response['Users']
-    print('found = {}'.format(found))
-
     total_results = len(found)
-    print('total_results = {}'.format(total_results))
-
     rv = ListResponse(found,
                       start_index=start_index,
                       count=count,
@@ -320,20 +275,11 @@ def users_get():
     return flask.jsonify(rv.to_scim_resource())
 
 
-# to-do
-# @app.route("/scim/v2/Groups", methods=['GET'])
-# def groups_get():
-#     rv = ListResponse([])
-#     return flask.jsonify(rv.to_scim_resource())
-
-
-# if __name__ == "__main__":
-#     try:
-#         User.query.one()
-#     except:
-#         db.create_all()
-#     app.debug = True
-#     socketio.run(app)
+@app.route("/scim/v2/Groups", methods=['GET'])
+def groups_get():
+    # TODO: implement GET Groups
+    rv = ListResponse([])
+    return flask.jsonify(rv.to_scim_resource())
 
 
 if __name__ == "__main__":
